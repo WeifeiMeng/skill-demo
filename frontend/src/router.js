dae654813fcd73a81a26dd22a26222a80c6ac25a
vicmeng <vicmengcs@163.com>
2026-06-14 15:42:04 +0800

    feat: add /challenges route and set Landing as home page

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Landing from './Landing.vue'
 import Home from './Home.vue'
 import Exam from './Exam.vue'
 import Completed from './Completed.vue'
 
 const routes = [
-  { path: '/', name: 'home', component: Home },
+  { path: '/', name: 'landing', component: Landing },
+  { path: '/challenges', name: 'challenges', component: Home },
   { path: '/exam/:filename', name: 'exam', component: Exam },
   { path: '/completed/:filename', name: 'completed', component: Completed }
 ]
